f1d37b2a9717b7b033fffc588a8bcf5a644647c1
Chris Tankersley <chris@ctankersley.com>
2013-04-10 18:58:20 -0400

    Added tests

---
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use ApplicationTest\Bootstrap;
+use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
+use Application\Controller\IndexController;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use PHPUnit_Framework_TestCase;
+
+class IndexControllerTest extends \PHPUnit_Framework_TestCase
+{
+   public function testContactActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'contact');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    public function testCopyrightActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'copyright');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    public function testPrivacyActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'privacy');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
